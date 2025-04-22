433a1fe64c93b4cfcc698c04d3edba24f47540ce
Adrián Gil <adriangn@users.noreply.github.com>
2025-04-22 18:08:49 +0200

    chore(dev): deshabilitar StrictMode para evitar doble renderizado en desarrollo

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // Comentamos StrictMode temporalmente para evitar el doble renderizado en desarrollo
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
